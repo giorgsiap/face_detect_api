d728c48493f5ef4e87f57e76fcbbd70fd8ef1b11
giorgsiap <giorgsiap@hotmail.gr>
2020-07-15 12:58:14 +0300

    commiting api

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : '8861',
+    database : 'smart-brain'
+  }
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => { res.send(database.users) });
+
+//===/signin --> POST =	success/fail===
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
+
+//===/register -->	POST = user===
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
+
+//===/profile/:userId --> GET =	user===
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) });
+
+//===/image -->	PUT = user
+app.put('/image', (req, res) => { image.handleImage(req, res, db) });
+
+//===/API call -->	post
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
+
+
+app.listen(3000, () => {
+	console.log('app is running on port 3000');
+});
